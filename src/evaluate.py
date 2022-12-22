033b9f69f29e9a18b901dc1d7f4b1832c2e96367
thatprogrammer1 <thatprogrammer1@gmail.com>
2022-12-22 07:24:50 +0000

    split demo and evaluate script

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,5 +1,4 @@
 
-
 import argparse
 from dataclasses import dataclass
 import logging
@@ -19,7 +18,7 @@ class NoDiffIKWarnings(logging.Filter):
 logging.getLogger("drake").addFilter(NoDiffIKWarnings())
 
 
-def stacking_demo(prism_config: PrismConfig, seed=None):
+def stacking_demo(prism_config: PrismConfig, max_stacked_trials, trial_num=0,  seed=None):
     meshcat.Delete()
     if seed == None:
         seed = np.random.randint(10000000)
@@ -60,24 +59,50 @@ def stacking_demo(prism_config: PrismConfig, seed=None):
     # run as fast as possible
     simulator.set_target_realtime_rate(0)
     meshcat.AddButton("Stop Simulation", "Escape")
-    print("Press Escape to stop the simulation")
+    # print("Press Escape to stop the simulation")
+    max_stacked = 1
     while meshcat.GetButtonClicks("Stop Simulation") < 1:
         if simulator.get_context().get_time() > 60 * sum(prism_config):
             raise Exception("Took too long")
         simulator.AdvanceTo(simulator.get_context().get_time() + 2.0)
-        # stats = diagram.get_output_port().Eval(simulator.get_context())
+        stats = diagram.get_output_port().Eval(simulator.get_context())
+        max_stacked = max(stats["max_stacked"], max_stacked)
+        max_stacked_trials[trial_num] = max_stacked
+        if stats["max_stacked"] == sum(prism_config):
+            return 1
         visualizer.StopRecording()
     visualizer.PublishRecording()
     meshcat.DeleteButton("Stop Simulation")
     return 0
 
 
+def record_success_rate(prism_config):
+    print("Recording with prism config: ", prism_config)
+    with open('logs/' + str(prism_config)+'.log', 'w') as f:
+        f.write(f"Recording with prism config: {prism_config}\n")
+    cnt = 0
+    max_stacked_trials = [0]*10
+    for i in range(10):
+        try:
+            cnt += stacking_demo(prism_config, max_stacked_trials, i)
+        except Exception as e:
+            with open('logs/' + str(prism_config)+'.log', 'a') as f:
+                f.write(f"exception occured, {e}")
+                f.write("\n")
+            print("Exception occured: ", e)
+        print(f"Success rate: {cnt}/{i+1}")
+        print("Max stacked:", max_stacked_trials[:i+1])
+        with open('logs/' + str(prism_config)+'.log', 'a') as f:
+            f.write(f"Success rate: {cnt}/{i+1}\n")
+            f.write(f"Max stacked: {max_stacked_trials[:i+1]}\n")
+
+
 if __name__ == "__main__":
     meshcat = StartMeshcat()
     parser = argparse.ArgumentParser(
-        prog='Demo stacking robot')
+        prog='Evaluate stacking robot')
     parser.add_argument('-r', '--rect', default='0')
     parser.add_argument('-p', '--pent', default='0')
 
     args = parser.parse_args()
-    stacking_demo(PrismConfig(0, int(args.rect), int(args.pent)))
+    record_success_rate(PrismConfig(0, int(args.rect), int(args.pent)))
